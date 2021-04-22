f07ecf3f980c96e3362abd9453f6a3dc94bb676d
boury_a <boury_a@etna-alternance.net>
2021-04-22 23:18:11 +0200

    fix IA choosing

--- NetworkShipBattle/computer.py
+++ NetworkShipBattle/computer.py
@@ -14,13 +14,13 @@ class Computer:
         self.submarine = 1
 
     def terminal_board_creation(self):
-            # while True:
-            for x in range(self.board_size):
-                self.computer_board.append(["O"]*self.board_size)
-            for x in self.computer_board:
-                print("  "+"- "*self.board_size)
-                print("| "+" ".join(x)+" |")
+        # while True:
+        for x in range(self.board_size):
+            self.computer_board.append(["O"]*self.board_size)
+        for x in self.computer_board:
             print("  "+"- "*self.board_size)
+            print("| "+" ".join(x)+" |")
+        print("  "+"- "*self.board_size)
 
     def getter_game_board(self):
         self.game_board = Game.getter_game_board()
@@ -42,9 +42,13 @@ class Computer:
         self.computer_board[shot_ship_row][shot_ship_col] = "X"
 
     def choose_random_ships_positions(self):
-        counter = 0
-        i = 0
-        while counter < len(self.ship_list):
+        counter = -1
+        letter_list = ["A", "C", "D", "T", "S"]
+        # while list of ships is not over
+        while counter < len(self.ship_list)-1:
+            i = 0
+            y = 0
+            counter += 1
             if self.ship_list[counter] == 5:
                 letter = "A"
             if self.ship_list[counter] == 4:
@@ -55,56 +59,47 @@ class Computer:
                 letter = "T"
             if self.ship_list[counter] == 1:
                 letter = "S"
+            ship_row = randint(0, len(self.computer_board))
+            ship_col = randint(0, len(self.computer_board[0]))
+            choice_position = choice(["width", "height"])
             while i < self.ship_list[counter]:
                 i += 1
-                ship_row = randint(0, len(self.computer_board) - 1)
-                ship_col = randint(0, len(self.computer_board[0]) - 1)
+                if choice_position == "width":
+                    if self.computer_board[ship_row][ship_col+i]:
+                        if self.computer_board[ship_row][ship_col] in letter_list:
+                            i -= 1
+                            continue
+                        self.computer_board[ship_row][ship_col] == letter
+                        if self.computer_board[ship_row][ship_col] in letter_list:
+                            y += 1
+                            self.computer_board[ship_row][ship_col-y] = letter
 
-                random_choice_row = choice([-1, 1])
-                random_choice_col = choice([-1, 1])
-                first_coordonate_computer_choice = choice([-1, 1])
+                        self.computer_board[ship_row][ship_col+i] = letter
+                        self.display_computer_board()
+                    else:
+                        y += 1
+                        self.computer_board[ship_row][ship_col-y]
+                if choice_position == "height":
+                    if self.computer_board[ship_row+i][ship_col]:
+                        if self.computer_board[ship_row][ship_col] in letter_list:
+                            i -= 1
+                            continue
+                        self.computer_board[ship_row][ship_col] == letter
+                        if self.computer_board[ship_row][ship_col] in letter_list:
+                            y += 1
+                            self.computer_board[ship_row-y][ship_col] = letter
 
-                if first_coordonate_computer_choice == 1:
-                    if random_choice_row == 1:
-                        if self.computer_board[[ship_row+i][ship_col]] == "O":
-                            self.computer_board[[ship_row+i]
-                                                [ship_col]] == letter
-                    if random_choice_row == -1:
-                        if self.computer_board[[ship_row-i][ship_col]] == "O":
-                            self.computer_board[[ship_row-i]
-                                                [ship_col]] == letter
-                    if random_choice_col == 1:
-                        if self.computer_board[[ship_row][ship_col+i]] == "O":
-                            self.computer_board[[ship_row]
-                                                [ship_col+i]] == letter
-                    if random_choice_col == -1:
-                        if self.computer_board[[ship_row][ship_col-i]] == "O":
-                            self.computer_board[[ship_row+i]
-                                                [ship_col-i]] == letter
-                if first_coordonate_computer_choice == -1:
-                    if random_choice_col == 1:
-                        if self.computer_board[[ship_row][ship_col+i]] == "O":
-                            self.computer_board[[ship_row]
-                                                [ship_col+i]] == letter
-                    if random_choice_col == -1:
-                        if self.computer_board[[ship_row][ship_col-i]] == "O":
-                            self.computer_board[[ship_row+i]
-                                                [ship_col-i]] == letter
-                    if random_choice_row == 1:
-                        if self.computer_board[[ship_row+i][ship_col]] == "O":
-                            self.computer_board[[ship_row+i]
-                                                [ship_col]] == letter
-                    if random_choice_row == -1:
-                        if self.computer_board[[ship_row-i][ship_col]] == "O":
-                            self.computer_board[[ship_row-i]
-                                                [ship_col]] == letter
-            counter += 1
+                        self.computer_board[ship_row+i][ship_col] = letter
+                        self.display_computer_board()
+                    else:
+                        y += 1
+                        self.computer_board[ship_row-y][ship_col]
 
 
 #g = Game(10)
-#g.terminal_board_creation()
+# g.terminal_board_creation()
 c = Computer(10)
 c.terminal_board_creation()
-#c.computer_fire_choice()
+# c.computer_fire_choice()
 c.choose_random_ships_positions()
 c.display_computer_board()
