d8f7513ca6700fffc1b9ce8556915c2a2d26abc0
boury_a <boury_a@etna-alternance.net>
2021-05-02 20:31:52 +0200

    write player choice

--- NetworkShipBattle/computer.py
+++ NetworkShipBattle/computer.py
@@ -1,9 +1,11 @@
 from random import choice, randint
 
+
 class Computer:
     def __init__(self, board_size):
         self.board_size = board_size
         self.computer_board = []
+        self.computer_target_board = []
         self.ship_list = [5, 4, 3, 3, 2, 1]
         self.aircraft_carrier = 5
         self.cruiser = 4
@@ -11,7 +13,8 @@ class Computer:
         self.frigate = 3
         self.torpedo_boat = 2
         self.submarine = 1
-        self.letter_list = ["A", "C", "D","F", "T", "S"]
+        self.letter_list = ["A", "C", "D", "F", "T", "S"]
+
     def terminal_board_creation(self):
         # while True:
         for x in range(self.board_size):
@@ -20,6 +23,10 @@ class Computer:
             print("  "+"- "*self.board_size)
             print("| "+" ".join(x)+" |")
         print("  "+"- "*self.board_size)
+
+    def computer_target_board_creation(self):
+        for x in range(self.board_size):
+            self.computer_target_board.append(["O"]*self.board_size)
 
     def getter_game_board(self):
         self.game_board = Game.getter_game_board()
@@ -35,70 +42,80 @@ class Computer:
             print("| "+" ".join(x)+" |")
         print("  "+"- "*self.board_size)
 
+    def display_computer_target__board(self):
+        for x in self.computer_target_board:
+            print("  "+"- "*self.board_size)
+            print("| "+" ".join(x)+" |")
+        print("  "+"- "*self.board_size)
+
     def computer_fire_choice(self):
-        shot_ship_row = randint(0, len(self.computer_board) - 1)
-        shot_ship_col = randint(0, len(self.computer_board[0]) - 1)
-        self.computer_board[shot_ship_row][shot_ship_col] = "X"
+        shot_ship_row = randint(0, len(self.computer_target_board) - 1)
+        shot_ship_col = randint(0, len(self.computer_target_board[0]) - 1)
+        if self.computer_target_board[shot_ship_row, shot_ship_col] != "X":
+            self.computer_target_board[shot_ship_row][shot_ship_col] = "X"
+        self.computer_fire_choice()
 
     def choose_random_ships_positions(self):
         counter = -1
-        
+
         # while list of ships is not over
         while counter < len(self.ship_list)-1:
             i = 0
             y = 0
             counter += 1
-            letter=self.letter_list[counter]
+            letter = self.letter_list[counter]
             ship_row = randint(0, len(self.computer_board)-1)
             ship_col = randint(0, len(self.computer_board[0])-1)
             choice_position = choice(["width", "height"])
-            #choice_position="width"
+            # choice_position="width"
             while i < self.ship_list[counter]:
-                #CHECK INDEXERROR AND CLEAN UP THE SHIP
+                # CHECK INDEXERROR AND CLEAN UP THE SHIP
                 try:
                     i += 1
                     if choice_position == "width":
-                        #CHECK IF LOCATION IS TAKEN BY ANOTHER SHIP
-                        if self.computer_board[ship_row][ship_col]!="O":
-                            #CHECK IF LOCATION IS ALREADY TAKEN BY THE SHIP
-                            if self.computer_board[ship_row][ship_col]==letter:
-                                #IF TAKEN BY THE SHIP SEEK WHERE TO EXTEND IT
-                                if self.computer_board[ship_row][ship_col+y]=="O":
-                                    self.computer_board[ship_row][ship_col+y]=letter 
+                        # CHECK IF LOCATION IS TAKEN BY ANOTHER SHIP
+                        if self.computer_board[ship_row][ship_col] != "O":
+                            # CHECK IF LOCATION IS ALREADY TAKEN BY THE SHIP
+                            if self.computer_board[ship_row][ship_col] == letter:
+                                # IF TAKEN BY THE SHIP SEEK WHERE TO EXTEND IT
+                                if self.computer_board[ship_row][ship_col+y] == "O":
+                                    self.computer_board[ship_row][ship_col+y] = letter
                                     self.display_computer_board()
-                                    y+=1
+                                    y += 1
                                     continue
-                                if self.computer_board[ship_row][ship_col-y]=="O":
-                                    self.computer_board[ship_row][ship_col-y]=letter
+                                if self.computer_board[ship_row][ship_col-y] == "O":
+                                    self.computer_board[ship_row][ship_col-y] = letter
                                     self.display_computer_board()
-                                    y+=1
+                                    y += 1
                                     continue
                                 raise IndexError
                             raise IndexError
-                        self.computer_board[ship_row][ship_col]=letter
+                        self.computer_board[ship_row][ship_col] = letter
                         self.display_computer_board()
-                        y+=1
+                        y += 1
                     if choice_position == "height":
-                        #CHECK IF LOCATION IS TAKEN BY ANOTHER SHIP
-                        if self.computer_board[ship_row][ship_col]!="O":
-                            #CHECK IF LOCATION IS ALREADY TAKEN BY THE SHIP
-                            if self.computer_board[ship_row][ship_col]==letter:
-                                #IF TAKEN BY THE SHIP SEEK WHERE TO EXTEND IT
-                                if self.computer_board[ship_row+y][ship_col]=="O":
-                                    self.computer_board[ship_row+y][ship_col]=letter 
+                        # CHECK IF LOCATION IS TAKEN BY ANOTHER SHIP
+                        if self.computer_board[ship_row][ship_col] != "O":
+                            # CHECK IF LOCATION IS ALREADY TAKEN BY THE SHIP
+                            if self.computer_board[ship_row][ship_col] == letter:
+                                # IF TAKEN BY THE SHIP SEEK WHERE TO EXTEND IT
+                                if self.computer_board[ship_row+y][ship_col] == "O":
+                                    self.computer_board[ship_row +
+                                                        y][ship_col] = letter
                                     self.display_computer_board()
-                                    y+=1
+                                    y += 1
                                     continue
-                                if self.computer_board[ship_row-y][ship_col]=="O":
-                                    self.computer_board[ship_row-y][ship_col]=letter
+                                if self.computer_board[ship_row-y][ship_col] == "O":
+                                    self.computer_board[ship_row -
+                                                        y][ship_col] = letter
                                     self.display_computer_board()
-                                    y+=1
+                                    y += 1
                                     continue
                                 raise IndexError
                             raise IndexError
-                        self.computer_board[ship_row][ship_col]=letter
+                        self.computer_board[ship_row][ship_col] = letter
                         self.display_computer_board()
-                        y+=1
+                        y += 1
                 except IndexError:
                     for nested_list in self.computer_board:
                         for index, e in enumerate(nested_list):
--- NetworkShipBattle/game.py
+++ NetworkShipBattle/game.py
@@ -6,14 +6,19 @@ from network import Network_connection
 class Game:
     def __init__(self, board_size):
         self.running = True
-        self.game_board = []
         self.board_size = board_size
+        self.game_board = []
+        self.target_board = []
         self.ship_list = [5, 4, 3, 3, 2, 1]
         self.aircraft_carrier = 5
         self.cruiser = 4
-        self.destroyer = 3  # *2
+        self.destroyer = 3
+        self.frigate = 3
         self.torpedo_boat = 2
         self.submarine = 1
+        self.letter_list = ["A", "C", "D", "F", "T", "S"]
+        self.ships_name = ["aircraft_carrier", "cruiser",
+                           "destroyer", "destroyer", "torpedo_boat", "submarine"]
 
     def getter_game_board(self):
         return self.game_board
@@ -25,37 +30,58 @@ class Game:
         print("REALOADED! PREPARE TO FIRE!")
         row_shot = input("choose a row: ")
         col_shot = input("choose a col: ")
-        self.game_board[row_shot][col_shot] = "X"
+        if self.target_board[row_shot][col_shot] != "X":
+            self.target_board[row_shot][col_shot] = "X"
+            return self.target_board[row_shot][col_shot]
+        print("ERROR choose a correct target!")
+        self.player_fire_choice()
 
     def player_choose_ship_positions(self):
         counter = -1
-        ship_list = [5, 4, 3, 3, 2, 1]
-        letter_list = ["A", "C", "D", "D", "T", "S"]
-        ships_name = ["aircraft_carrier", "cruiser",
-                      "destroyer", "destroyer", "torpedo_boat", "submarine"]
         while counter < len(self.ship_list)-1:
-            i=0
-            # if self.ship_list[counter] == 5:
-            #     letter = "A"
-            # if self.ship_list[counter] == 4:
-            #     letter = "C"
-            # if self.ship_list[counter] == 3:
-            #     letter = "D"
-            # if self.ship_list[counter] == 2:
-            #     letter = "T"
-            # if self.ship_list[counter] == 1:
-            #     letter = "S"
-            print(f"COMMANDER DEFINE POSITION OF YOUR {ships_name[counter]}")
-            while i<ship_list[counter]:
+            i = 0
+            y = 0
+            counter += 1
+            letter = self.letter_list[counter]
+            while i < ship_list[counter]:
                 try:
-                    i+=1
+                    print(
+                        f"COMMANDER DEFINE POSITION OF YOUR {self.ships_name[counter].upper()}")
+                    i += 1
                     ship_row = input("position your ship on row: ")
                     ship_col = input("position yout ship on col: ")
-                    self.game_board[ship_row][ship_col]=letter_list[counter]
+                    if self.game_board[ship_row][ship_col] != "O":
+                        i -= 1
+                        # self.display_computer_board()
+                        print(
+                            "ERROR: stop drinking captain another ship is already here!")
+                    #if i > 1:
+                        if self.game_board[ship_row+y][ship_col] != letter:
+                            print(
+                                "ERROR: you can't put the ship at several positions you drunker!")
+                            i -= 1
+                            continue
+                        if self.game_board[ship_row-y][ship_col] != letter:
+                            print(
+                                "ERROR: you can't put the ship at several positions you drunker!")
+                            i -= 1
+                            continue
+                        if self.game_board[ship_row][ship_col+y] != letter:
+                            print(
+                                "ERROR: you can't put the ship at several positions you drunker!")
+                            i -= 1
+                            continue
+                        if self.game_board[ship_row+y][ship_col-y] != letter:
+                            print(
+                                "ERROR: you can't put the ship at several positions you drunker!")
+                            i -= 1
+                            continue
+                    self.game_board[ship_row][ship_col] = letter
+                    self.display_board()
+                    y += 1
                 except IndexError:
-                    i-=1
-                    self.display_computer_board()
-                    print("ERROR choose a valide position!")
+                    i -= 1
+                    print("ERROR: you are out of range captain don't do this!")
                     continue
 
     def terminal_board_creation(self):
@@ -67,8 +93,23 @@ class Game:
             print("| "+" ".join(x)+" |")
         print("  "+"- "*self.board_size)
 
+    def terminal_target_board_creation(self):
+        # while True:
+        for x in range(self.board_size):
+            self.target_board.append(["O"]*self.board_size)
+        for x in self.target_board:
+            print("  "+"- "*self.board_size)
+            print("| "+" ".join(x)+" |")
+        print("  "+"- "*self.board_size)
+
     def display_board(self):
         for x in self.game_board:
+            print("  "+"- "*self.board_size)
+            print("| "+" ".join(x)+" |")
+        print("  "+"- "*self.board_size)
+
+    def display_target_board(self):
+        for x in self.target_board:
             print("  "+"- "*self.board_size)
             print("| "+" ".join(x)+" |")
         print("  "+"- "*self.board_size)
